2f1ef62a67a9ed987c42d4ddcc9ac2916d66d36f
EDWARD KERIESHEK <karanjaeduh@gmail.com>
2023-04-05 08:46:34 -0700

    mn

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -10,4 +10,4 @@ int factorial(int n);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
-#endif /* MAIN_H */
+#endif 
